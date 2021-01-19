60f91d36dbf02557789fbb0b925d7f2507a4c5ec
유시현 <kamuel@qdillion.com>
2021-01-20 01:23:15 +0900

    - 운동 카운트 추가

---
+++ app/src/main/java/com/exercise/health_exercise/ui/activitys/ExerciseActivity.kt
@@ -0,0 +1,11 @@
+package com.exercise.health_exercise.ui.activitys
+
+import android.os.Bundle
+import com.exercise.health_exercise.R
+
+class ExerciseActivity :BaseActivity(){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_exercise)
+    }
+}
