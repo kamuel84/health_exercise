b0192c1c4d6b3e4481711c3adbbb6448d45dfa6d
유시현 <kamuel@qdillion.com>
2022-02-06 13:11:39 +0900

    - 사용자 생성 리스트가 먼저 나오도록 수정

--- app/src/main/java/com/exercise/health_exercise/data/health_list/HealthListDao.kt
+++ app/src/main/java/com/exercise/health_exercise/data/health_list/HealthListDao.kt
@@ -14,7 +14,7 @@ interface HealthListDao : BaseDao<HealthListData> {
     @Query("SELECT * FROM health_list WHERE idx = :index")
     fun getAll(index:Long) : LiveData<List<HealthListData>>
 
-    @Query("SELECT * FROM health_list WHERE list_type = 'C' OR list_type ='CD'")
+    @Query("SELECT * FROM health_list WHERE list_type = 'C' OR list_type ='CD' ")
     fun getCustomList() : LiveData<List<HealthListData>>
 
     @Query("SELECT idx FROM health_list ORDER BY idx DESC LIMIT 1")
@@ -36,7 +36,7 @@ interface HealthListDao : BaseDao<HealthListData> {
     @Query("SELECT health_list.*, health_list_items.health_Photo, health_list_items.item_count, 0 AS 'isChecked' FROM health_list " +
             "LEFT JOIN (SELECT *, count(*) as item_count FROM health_list_items " +
             "INNER JOIN exercise ON health_list_items.health_index = exercise.idx GROUP BY health_list_items.health_list_index) as health_list_items " +
-            "ON health_list.idx = health_list_items.health_list_index WHERE health_list.list_type = 'C' OR health_list.list_type = 'CD'")
+            "ON health_list.idx = health_list_items.health_list_index WHERE health_list.list_type = 'C' OR health_list.list_type = 'CD' ORDER BY list_type ASC")
     fun getCustomWithItem():LiveData<List<HealthListWithItemData>>
 
     @Query("DELETE FROM health_list WHERE idx = :index")
