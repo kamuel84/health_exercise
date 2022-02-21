060d2a66d0d4805a375e2afdaf4f48534f7acfe3
Sean <carrot0615@naver.com>
2022-02-21 22:28:12 +0900

    사이드 메뉴 싱크 안맞는 오류 수정

--- app/src/main/java/com/exercise/health_exercise/MainActivity.kt
+++ app/src/main/java/com/exercise/health_exercise/MainActivity.kt
@@ -175,7 +175,7 @@ class MainActivity : BaseActivity(), View.OnClickListener, HomeFragment.onHomeFr
             1 -> {
                 var fragment: HomeFragment = HomeFragment()
                 pushFragment(R.id.nav_host_fragment, fragment)
-                nav_view.menu.getItem(0).isChecked = true
+                nav_view.menu.getItem(1).isChecked = true
                 clMain_BottomMenu1.setBackgroundColor(
                     ContextCompat.getColor(
                         this,
@@ -199,7 +199,7 @@ class MainActivity : BaseActivity(), View.OnClickListener, HomeFragment.onHomeFr
             2 -> {
                 var fragment: CustomListFragment = CustomListFragment()
                 pushFragment(R.id.nav_host_fragment, fragment)
-                nav_view.menu.getItem(1).isChecked = true
+                nav_view.menu.getItem(0).isChecked = true
                 clMain_BottomMenu1.setBackgroundColor(
                     ContextCompat.getColor(
                         this,
